9cf05cd8f07f2fb95157dd44d04ffc67d69095d1
Ubuntu <ubuntu@ip-172-31-24-49.ec2.internal>
2021-03-29 17:31:42 +0000

    Usuari i vista login/register

--- HackerNews_G14/config/routes.rb
+++ HackerNews_G14/config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  resources :users do
+    member do
+      post 'login'
+    end
+  end
+  
+  
+ # scope 'news' do
+#    resources :contribucios, as: 'news'
+#  end
   resources :contribucios do
     member do
       put 'like'
@@ -7,7 +17,6 @@ Rails.application.routes.draw do
       get 'news'
     end
   end
-  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'contribucios#index'
 end
--- HackerNews_G14/db/schema.rb
+++ HackerNews_G14/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_26_120011) do
+ActiveRecord::Schema.define(version: 2021_03_29_155126) do
 
   create_table "contribucios", force: :cascade do |t|
     t.string "tittle"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 2021_03_26_120011) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "likes"
     t.integer "like", default: 0
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
