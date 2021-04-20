550786fa3b4f3a6f469351480e418cb5bd59b4fb
Ubuntu <ubuntu@ip-172-31-27-142.ec2.internal>
2021-04-20 17:44:36 +0000

    Login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,11 @@
 class SessionsController < ApplicationController
+    def omniauth
+       user = User.from_omniauth(request.env['omniauth.auth'])
+       if user.valid?
+           session[:user_id] = user.index
+           redirect_to user_path(user_id)
+       else
+           redirect_to '/login'
+       end
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    def self.form_omniauth(response)
+        User.find_or_create_by(index response[:index]) do |u|
+            u.username = response[:info][:name]
+            u.password = response[:info][:password]
+        end 
+    end
 end
