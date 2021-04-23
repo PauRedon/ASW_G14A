cec698ad2c6fe15d46a6c78cf0929e801a143b0e
Ubuntu <ubuntu@ip-172-31-24-49.ec2.internal>
2021-04-23 00:08:22 +0000

    Migrations

--- db/migrate/20210422193759_add_self_relation_to_comments.rb
+++
@@ -1,7 +0,0 @@
-class AddSelfRelationToComments < ActiveRecord::Migration[6.1]
-  def change
-    change_table :comments do |t|
-      t.references :parent_comment, foreign_key: {to_table: :comments}
-    end
-  end
-end
--- db/migrate/20210422195120_remove_parent_comment_col.rb
+++
@@ -1,5 +0,0 @@
-class RemoveParentCommentCol < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :comments, :parent_comment_id
-  end
-end
