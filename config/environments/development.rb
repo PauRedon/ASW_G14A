d1a71d2e43bbb4318e60074dca6215ee25f28b49
Ubuntu <ubuntu@ip-172-31-53-2.ec2.internal>
2021-05-11 18:29:32 +0000

    Heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
 
-  config.hosts << "hacker-news-g14a.herokuapp.com"
+  config.hosts << "protected-earth-28993.herokuapp.com"
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.hosts.clear
